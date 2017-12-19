4697f70c836c4a7ec13bc539f7c0196515ebcbef
Micah <micah.mpc@gmail.com>
2017-12-19 12:02:17 -0800

    added solution to be worked on

--- scripts/album.js
+++ scripts/album.js
@@ -87,6 +87,16 @@ var albumPicasso = {
      }
  };
 
+ var findParentByClassName = function(element, targetClass) {
+    if (element) {
+        var currentParent = element.parentElement;
+        while (currentParent.className !== targetClass && currentParent.className !== null) {
+            currentParent = currentParent.parentElement;
+        }
+        return currentParent;
+    }
+};
+
  var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 
 window.onload = function() {
